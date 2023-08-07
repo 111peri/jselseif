e5833313d5f71364664787d5a1d0bf3073418d2b
111peri <perizat.melisova.kk@gmail.com>
2023-08-07 21:25:05 +0600

    add js else if

---
+++ js/index.js
@@ -0,0 +1,42 @@
+// const x = 7
+
+// if (x === 7) {
+//     alert("верно")
+// } else {
+//     alert("неверно")
+// }
+
+// const a = 1
+
+// const b = 5
+// if (a >= 1 && b >= 3) {
+//     const sum = a + b
+
+//     console.log(sum)
+// } else {
+//     const result = a - b
+
+//     console.log(result)
+// }
+
+// const city = prompt("Столица Кыргызстана");
+
+// if (city === "Бишкек") {
+//     console.log("Верно!")
+// } else {
+//     console.log("Не знаете?")
+// }
+
+// const time = 30
+
+// if (time >= 0 && time <= 14) {
+//     console.log("Первая четверть");
+// } else if (time >= 15 && time <= 29) {
+//     console.log("Вторая четверть");
+// } else if (time >= 30 && time <= 44) {
+//     console.log("Третья четверть");
+// } else if (time >= 45 && time <= 59) {
+//     console.log("Четвертая четверть");
+// } else {
+//     console.log("Значение не попадает в диапазон от 0 до 59")
+// }
